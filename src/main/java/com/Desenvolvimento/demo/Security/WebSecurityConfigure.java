dfc97adf6de8b45292923eb57e2c08e44df9fb80
Maciel-Dev <jvmacielv@gmail.com>
2023-02-15 09:07:27 -0300

    Dummy Commits and Configuration With Webservice

--- src/main/java/com/Desenvolvimento/demo/Security/WebSecurityConfigure.java
+++ src/main/java/com/Desenvolvimento/demo/Security/WebSecurityConfigure.java
@@ -2,12 +2,16 @@ package com.Desenvolvimento.demo.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Service;
 
 @EnableWebSecurity
@@ -25,10 +29,29 @@ public class WebSecurityConfigure {
         return creator;
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests().anyRequest().hasRole("USER")
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/index", true)
+                .and()
+                .logout();
+        return http.build();
+    }
+
 
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
